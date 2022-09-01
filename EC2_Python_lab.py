aac6c70649400e6b1a1bb2b74d0371bbd108f379
jgreenwi.28 <jgreenwi.28@gmail.com>
2022-09-01 03:24:53 +0000

    commit updates

--- EC2_Python_lab.py
+++ EC2_Python_lab.py
@@ -1,5 +1,5 @@
 #EC2 + Boto + Python : you can use Boto3 to create/configure/and manage AWS services
-#You are tasked with creating a list using Python that curates unique EC2 that users can attach to instances. Python scripts should [allow users to input how many EC2 instances they want names for and output the same amount of names.] 
+#You are tasked with creating a list using Python that curates unique EC2 names that users can attach to instances. Python scripts should [allow users to input how many EC2 instances they want names for and output the same amount of names.] 
 #Also, [allow users to input the name of their department that is used with the unique name]
 #Then, [Generate random characters that will be included in the unique name]
 
@@ -7,15 +7,23 @@
 #s3 = boto3.resource('s3')
 import random
 import string
-aws_ec2 = []
+from time import sleep
+
+#create list of EC2 unique names
+aws_ec2_name = ["Accounting", "FinOps", ]
+aws_ec2_name.append(random.choice)
+print(radomn.choice(aws_ec2_name)
 
 letters= string.ascii_letters
 x ="".join(random.sample(letters,5))
 print(x)
 
-aws_ec2_name= (x)
-print(x)
 
+#Complex: asks department name with string clarification 
+sleep(3)
 aws_ec2_name = input("What is your department name?")
 print(input)
 
+sample_text ="FinOps"
+result = string.capwords(sample_text)
+print(result)
