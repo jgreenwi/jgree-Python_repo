8a4efb213cc2a1ba7202faa571dc85633daa8ec2
jgreenwi.28 <jgreenwi.28@gmail.com>
2022-09-09 17:53:44 +0000

    uploading file code

--- Labs/s3/s3-fileupload.py
+++ Labs/s3/s3-fileupload.py
@@ -1,1 +1,25 @@
 #AWS automation using boto3: How to upload a file to s3
+import boto3
+s3_resource=boto3.client("s3")
+s3_resource.upload_file(
+    Filename="unsplash.jpg",
+    Bucket="jgree09",
+    Key="unsplash.jpg"
+    )
+    
+import os
+import glob
+
+cwd=os.getcwd()
+cwd=cwd+"/upload/"
+file=glob.glob(cwd+"*.png")
+#automation code:
+for file in files:
+    s3_resource=boto3.client("s3")
+    s3_resource.upload_file(
+    Filename=file,
+    Bucket="jgree09",
+    Key=file.split("/")(-1)
+    )
+files[0].split("/")(-1)
+#the above command list files and presents the last item
