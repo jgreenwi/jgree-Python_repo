cab5b3f8227d562ca94c367b8726062afa0053c3
jgreenwi.28 <jgreenwi.28@gmail.com>
2022-09-10 22:14:27 +0000

    commit changes

--- Labs/Python Basics/lab_4_step_1_positional_arguments.py
+++ Labs/Python Basics/lab_4_step_1_positional_arguments.py
@@ -16,7 +16,7 @@ def main():
 if __name__=="__main__":
     main()
     
-    
+#Keyword arguments:     
 #import boto3
 
 def translate_text(text, source_language_code, target_language_code): # we define the positional arguments in the ()
@@ -33,3 +33,35 @@ def main():
 
 if __name__=="__main__":
     main()
+
+import boto3
+
+def translate_text(text, source_language_code, target_language_code): 
+    client = boto3.client('translate')
+    response = client.translate_text(
+        Text=text, 
+        SourceLanguageCode=source_language_code, 
+        TargetLanguageCode=target_language_code
+    )
+    print(response) 
+
+def main():
+    translate_text('I am learning to code in AWS','en','fr')
+
+if __name__=="__main__":
+    main()
+
+    
+#import boto3
+
+def translate_text(**kwargs): 
+    client = boto3.client('translate')
+    response = client.translate_text(**kwargs)
+    print(response) 
+
+def main():
+    translate_text(Text='I am learning to code in AWS',SourceLanguageCode='en',TargetLanguageCode='fr')
+
+if __name__=="__main__":
+    main()
+
