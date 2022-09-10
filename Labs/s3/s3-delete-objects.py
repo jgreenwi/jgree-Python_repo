2c2d7e2f872fd5bd6fb12b265320971d6dc22615
jgreenwi.28 <jgreenwi.28@gmail.com>
2022-09-10 01:18:53 +0000

    added automation code

--- Labs/s3/s3-delete-objects.py
+++ Labs/s3/s3-delete-objects.py
@@ -3,5 +3,19 @@ import boto3
 s3_resource=boto3.client("s3")
 s3_resource.client.delete_object(
     Bucket='jgree09'
-    Key="unsplash.jpg"
+    key ="unsplash.jpg"
     )
+print(response)
+#delete multiple objects
+import os 
+import glob
+
+#find all the obj from the bucket
+objects=s3_resource.list_objects(Bucket="jgree09")["Contents"]
+len(objects)
+
+#automation code
+for object in objects:  
+    s3_resource.delete_object(Bucket="jgree09" ,
+    key=object["Key"])
+    print(object["Key"])
