1cc745d6dd98c9c5571448e8a7761a76f5e9ec77
jgreenwi.28 <jgreenwi.28@gmail.com>
2022-09-10 04:08:32 +0000

    project

---
+++ Projects/DynamoDB-create-item.py
@@ -0,0 +1,71 @@
+import boto3
+
+dynamodb = boto3.client('dynamodb')
+
+response = dynamodb.put_item(
+    Item={
+   'ID':{
+          'S': 'hsv9krNOqy'  ,
+        },
+        'Theme Park': {
+            'S': 'Universal Studios',
+        },
+        'Ride': {
+            'S': 'Harry Potter',
+        },
+        'Rank': {
+            'S': '1',
+        },
+    'ID':{
+          'S': 'u2Kket8QKP'  ,
+        },
+        'Theme Park': {
+            'S': 'Hollywood Studios',
+        },
+        'Ride': {
+            'S': 'Tower of Terror',
+        },
+        'Rank': {
+            'S': '2',
+        },
+    'ID':{
+          'S': 'ouvGYwXPze'  ,
+        },
+        'Theme Park': {
+            'S': 'Hollywood Studios',
+        },
+        'Ride': {
+            'S': 'Galaxys Edge',
+        },
+        'Rank': {
+            'S': '3',
+        },
+    'ID':{
+          'S': 'FurxXXkfui'  ,
+        },
+        'Theme Park': {
+            'S': 'Animal Kingdom',
+        },
+        'Ride': {
+            'S': 'Flight of Passage',
+        },
+        'Rank': {
+            'S': '4',
+        },
+    'ID':{
+          'S': 'So1bPsGjst'  ,
+        },
+        'Theme Park': {
+            'S': 'Universal Studios',
+        },
+        'Ride': {
+            'S': 'Incredible Hulk',
+        },
+        'Rank': {
+            'S': '5',
+        },    
+    
+    },
+    ReturnConsumedCapacity='TOTAL',
+    TableName='DisneyAttractions',
+)
